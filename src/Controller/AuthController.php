1d2bf3555a3395ea3ef342156be289606ce64b98
CelienFio <celien.fiorelli@ynov.com>
2024-02-18 17:38:06 +0100

    init

---
+++ src/Controller/AuthController.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Persona;
+use App\Entity\User;
+use DateTime;
+use Doctrine\ORM\EntityManagerInterface;
+use Hateoas\UrlGenerator\UrlGeneratorInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Serializer\SerializerInterface;
+
+class AuthController extends AbstractController
+{
+    #[Route('/auth', name: 'app_auth')]
+    public function index(): JsonResponse
+    {
+        return $this->json([
+            'message' => 'Welcome to your new controller!',
+            'path' => 'src/Controller/AuthController.php',
+        ]);
+    }
+
+    #[Route('/api/register', name: 'auth.register', methods: ['POST'])]
+    public function register(Request $request, EntityManagerInterface $manager, UserPasswordHasherInterface $userPasswordHasher): JsonResponse
+    {
+        $dateNow = new DateTime();
+        $body = $request->toArray();
+        
+        $persona = new Persona();
+        $persona->setPhone($body['phone']);
+        $persona->setEmail($body['email']);
+        $persona->setCreatedAt($dateNow)->setUpdatedAt($dateNow);
+        $persona->setStatus('on');
+        
+        $manager->persist($persona);
+
+        $user = new User();
+        $user->setRoles(["USER"]);
+        $user->setUsername($body['username']);
+        $user->setPassword($userPasswordHasher->hashPassword($user, $body['password']));
+        $user->setPersona($persona);
+        $user->setCreatedAt($dateNow)->setUpdatedAt($dateNow);
+        $manager->persist($user);
+        
+        $manager->flush();
+        
+        return new JsonResponse(null, Response::HTTP_CREATED, [], true);
+    }
+
+}
